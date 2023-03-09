207fd27bd9fbe4a2c66b4e2ad933e50275e13227
smithpb <phillsm2@cisco.com>
2023-03-09 11:27:45 -0600

    add fakeAsync and tick

--- apps/test-app/src/app/app.component.spec.ts
+++ apps/test-app/src/app/app.component.spec.ts
@@ -1,4 +1,4 @@
-import { TestBed } from '@angular/core/testing';
+import { fakeAsync, TestBed, tick } from '@angular/core/testing';
 import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
 import { DummyModule } from '@harness-issue/dummy';
 import { DummyHarness } from '@harness-issue/dummy/testing';
@@ -13,16 +13,18 @@ describe('AppComponent', () => {
     }).compileComponents();
   });
 
-  it('should create the app', async () => {
+  it('should create the app', fakeAsync(async () => {
     const fixture = TestBed.createComponent(AppComponent);
     const loader = TestbedHarnessEnvironment.loader(fixture);
 
     const dummyHarness = await loader.getHarness(DummyHarness);
 
+    tick(1000);
+
     await expect(
       dummyHarness.matchesSelector('harness-issue-dummy')
     ).resolves.toBeTruthy();
-  });
+  }));
 
   it(`should have as title 'test-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
